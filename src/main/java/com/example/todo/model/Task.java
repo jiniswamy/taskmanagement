355061a21f907da344d5dc85e784421237e0515d
Ranjini Krishnaswamy <jinivasan@gmail.com>
2019-02-23 14:13:24 -0600

    changed Task id generation auto

--- src/main/java/com/example/todo/model/Task.java
+++ src/main/java/com/example/todo/model/Task.java
@@ -19,7 +19,7 @@ import lombok.Setter;
 public class Task {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer id;
 	
 	@Column
